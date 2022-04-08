42cd097b2a568cce21052b89eb9dacce46f9dbcb
Adam Kadlec <adam.kadlec@gmail.com>
2022-04-08 12:09:19 +0200

    Fixing async

--- fastybird_shelly_connector/connector.py
+++ fastybird_shelly_connector/connector.py
@@ -379,7 +379,7 @@ class ShellyConnector(IConnector):  # pylint: disable=too-many-instance-attribut
 
     # -----------------------------------------------------------------------------
 
-    def handle(self) -> None:
+    async def handle(self) -> None:
         """Run connector service"""
         if self.__stopped and not self.has_unfinished_tasks():
             self.__logger.warning("Connector is stopped and can't process another requests")
